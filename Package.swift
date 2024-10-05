3567df8432664910e6425e08e2256d3596644d86
Jasmin <4738799+jaeilers@users.noreply.github.com>
2024-10-05 10:25:53 +0200

    Migrate XCTest cases to Swift Testing

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,6 +23,5 @@ let package = Package(
             name: "JESideMenuControllerTests",
             dependencies: ["JESideMenuController"]
         )
-    ],
-    swiftLanguageVersions: [.v5]
+    ]
 )
